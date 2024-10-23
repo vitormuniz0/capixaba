586069f9eb2348b454191b19a2c57ba112bebfca
vitormuniz0 <vitormuniz311@gmail.com>
2024-10-22 23:03:52 -0300

    Comecando estilizacao do carrinho

--- capixaba/src/Components/Modal/index.js
+++ capixaba/src/Components/Modal/index.js
@@ -1,23 +1,25 @@
 import { Button } from "react-bootstrap";
 import { ModalCar } from "./style";
 
-const CustomModal = ({show , handleClose}) =>{
+const CustomModal = ({ show, handleClose }) => {
+  console.log("show vmodal", show);
 
-    console.log("show vmodal" , show)
-
-    return(
-        <ModalCar show={show} onHide={handleClose}>
-            <ModalCar.Header closeButton>
-                <ModalCar.Title>Modal</ModalCar.Title>
-            </ModalCar.Header>
-            <ModalCar.Body>
-                Modal exemplo
-            </ModalCar.Body>
-            <ModalCar.Footer>
-                <Button variant="secondary" onClick={handleClose}>Fechar</Button>
-            </ModalCar.Footer>
-        </ModalCar>
-    )
-}
+  return (
+    <ModalCar show={show} onHide={handleClose}>
+      <ModalCar.Header closeButton>
+        <ModalCar.Title>Suas Compras</ModalCar.Title>
+      </ModalCar.Header>
+      <ModalCar.Body>Modal exemplo</ModalCar.Body>
+      <ModalCar.Footer>
+        <Button variant="primary" >
+          Pedido
+        </Button>
+        <Button variant="secondary" onClick={handleClose}>
+          Fechar
+        </Button>
+      </ModalCar.Footer>
+    </ModalCar>
+  );
+};
 
 export default CustomModal;
--- capixaba/src/Components/Modal/style.js
+++ capixaba/src/Components/Modal/style.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
-import { Modal} from 'react-bootstrap';
+import { Modal } from "react-bootstrap";
 
 export const ModalCar = styled(Modal)`
-     .modal-content {
-    border-radius: 10px;  /* Borda arredondada */
+  .modal-content {
+    border-radius: 10px; /* Borda arredondada */
+    width: 100%;
   }
-  
+
+  .modal-title{
+    font-size: 25px;
+  }
+
+  .modal-body{
+    font-size: 20px;
+  }
   .modal-header {
-    border-bottom: none;  /* Remove a borda inferior */
+    border-bottom: none; /* Remove a borda inferior */
   }
-  
+
   .modal-footer {
-    border-top: none;  /* Remove a borda superior */
+    border-top: none; /* Remove a borda superior */
   }
-`
-
-
-
-
+`;
