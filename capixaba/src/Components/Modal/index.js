b94c6179891e4ec458e8af5e3fb93de0c741d724
vitormuniz0 <vitormuniz311@gmail.com>
2024-10-29 21:48:13 -0300

    Inserindo novos modals de confirmação do pedido e de inserir dados do cliente

--- capixaba/src/Components/Modal/index.js
+++ capixaba/src/Components/Modal/index.js
@@ -2,83 +2,247 @@ import {
   ButtonRemove,
   Buttons,
   ContainerButton,
+  ContainerEndereco,
   ContainerInput,
   ContentCar,
+  ContentInfo,
+  InfoPedido,
+  MessageTaxa,
   ModalCar,
+  PedidoInput,
   RadioInput,
   RadioLabel,
   SubContainer,
+  TitleProdutos,
 } from "./style";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsFillTrash3Fill } from "react-icons/bs";
 import { useState } from "react";
+import { ModalTitle } from "react-bootstrap";
 
 const CustomModal = ({ show, handleClose, cart, removeFromCart }) => {
   const totalPrice = cart.reduce((total, product) => total + product.price, 0);
 
-  const [deliveryMethod, setDeliveryMethod] = useState("retirada");
+  const [deliveryMethod, setDeliveryMethod] = useState("Retirada");
+  const [showSecondModal, setShowSecundModal] = useState(false);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
+  const [clientInfo, setClientInfo] = useState({
+    street: "", // Rua
+    neighborhood: "", // Bairro
+    number: "", // Número
+    complement: "", // Logradouro ou Complemento
+    name: "",
+    phone: "",
+  });
+
+  const resetClientInfo = () => {
+    setClientInfo({
+      street: "",
+      neighborhood: "",
+      number: "",
+      complement: "",
+      name: "",
+      phone: "",
+    });
+  };
 
   const handleDeliveryChange = (event) => {
     setDeliveryMethod(event.target.value);
   };
 
+  const hadleAdvance = () => {
+    if (deliveryMethod === "Delivery" && (clientInfo.street === "" || clientInfo.neighborhood === "" || clientInfo.number === "")) {
+      alert("Por favor, insira o endereço completo para a entrega.");
+      return; // Impede a abertura do segundo modal se o endereço não for preenchido
+    }
+    handleClose();
+    setShowSecundModal(true);
+  };
+
+  const handleCloseSecondModal = () => {
+    resetClientInfo(); // Limpa as informações ao fechar o segundo modal
+    setShowSecundModal(false);
+  };
+
+  const handleCloseConfirmationModal = () => {
+    setShowConfirmationModal(false);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setClientInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    setShowSecundModal(false);
+    setShowConfirmationModal(true);
+  };
+
   return (
-    <ModalCar show={show} onHide={handleClose}>
-      <ModalCar.Header closeButton>
-        <ModalCar.Title>
-          Suas Compras <AiOutlineShoppingCart />
-        </ModalCar.Title>
-      </ModalCar.Header>
-      <ModalCar.Body>
-        {cart.length === 0 ? (
-          <p>Seu Carrinho está Vazio!</p>
-        ) : (
-          cart.map((product, index) => (
-            <ContentCar>
-              <SubContainer>
-                <div key={index}>
-                  <h3>{product.nameProduct}</h3>
-                  <p>{product.desc}</p>
-                  <p>R$ {product.price},00</p>
-                </div>
-              </SubContainer>
-              <ContainerButton>
-                <ButtonRemove
-                  onClick={() => removeFromCart(product.id)}
-                >
-                 <BsFillTrash3Fill/>
-                </ButtonRemove>
-              </ContainerButton>
-            </ContentCar>
-          ))
-        )}
-        <h3>Total: R$ {totalPrice},00</h3>
-        <ContainerInput>        
-          <RadioInput
-            type="radio"
-            id="retirada"
-            value="retirada"
-            checked={deliveryMethod === "retirada"}
-            onChange={handleDeliveryChange}
-          />
-          <RadioLabel htmlFor="retirada">Retirada</RadioLabel>
-          <RadioInput
-            type="radio"
-            id="entrega"
-            value="entrega"
-            checked={deliveryMethod === "entrega"}
-            onChange={handleDeliveryChange}
-          />
-          <RadioLabel htmlFor="entrega">Delivery</RadioLabel>
-        </ContainerInput>
-      </ModalCar.Body>
-      <ModalCar.Footer>
-        <Buttons variant="primary">Avancar</Buttons>
-        <Buttons variant="secondary" onClick={handleClose}>
-          Fechar
-        </Buttons>
-      </ModalCar.Footer>
-    </ModalCar>
+    <>
+      <ModalCar show={show} onHide={() => {resetClientInfo(); handleClose()}}>
+        <ModalCar.Header closeButton>
+          <ModalCar.Title>
+            Suas Compras <AiOutlineShoppingCart />
+          </ModalCar.Title>
+        </ModalCar.Header>
+        <ModalCar.Body>
+          {cart.length === 0 ? (
+            <p>Seu Carrinho está Vazio!</p>
+          ) : (
+            cart.map((product, index) => (
+              <ContentCar>
+                <SubContainer>
+                  <div key={index}>
+                    <h3>{product.nameProduct}</h3>
+                    <p>{product.desc}</p>
+                    <p>R$ {product.price},00</p>
+                  </div>
+                </SubContainer>
+                <ContainerButton>
+                  <ButtonRemove onClick={() => removeFromCart(product.id)}>
+                    <BsFillTrash3Fill />
+                  </ButtonRemove>
+                </ContainerButton>
+              </ContentCar>
+            ))
+          )}
+          <h3>Total: R$ {totalPrice},00</h3>
+          {deliveryMethod === "Delivery" && (<MessageTaxa>*Consultar Taxa de Entrega Após Envio do Pedido*</MessageTaxa>)}
+          <ContainerInput>
+            <RadioInput
+              type="radio"
+              id="Retirada"
+              value="Retirada"
+              checked={deliveryMethod === "Retirada"}
+              onChange={handleDeliveryChange}
+            />
+            <RadioLabel htmlFor="Retirada">Retirada</RadioLabel>
+            <RadioInput
+              type="radio"
+              id="Delivery"
+              value="Delivery"
+              checked={deliveryMethod === "Delivery"}
+              onChange={handleDeliveryChange}
+            />
+            <RadioLabel htmlFor="Delivery">Delivery</RadioLabel>
+          </ContainerInput>
+          {deliveryMethod === "Delivery" && (
+            <ContainerEndereco>
+              <PedidoInput
+                type="text"
+                name="street"
+                value={clientInfo.street}
+                onChange={handleInputChange}
+                placeholder="Rua"
+              />
+              <PedidoInput
+                type="text"
+                name="neighborhood"
+                value={clientInfo.neighborhood}
+                onChange={handleInputChange}
+                placeholder="Bairro"
+              />
+              <PedidoInput
+                type="text"
+                name="number"
+                value={clientInfo.number}
+                onChange={handleInputChange}
+                placeholder="Numero"
+              />
+              <PedidoInput
+                type="text"
+                name="complement"
+                value={clientInfo.complement}
+                onChange={handleInputChange}
+                placeholder="Complemento"
+              />
+            </ContainerEndereco>
+          )}
+        </ModalCar.Body>
+        <ModalCar.Footer>
+          <Buttons variant="primary" onClick={hadleAdvance} >
+            Avancar
+          </Buttons>
+          <Buttons variant="secondary" onClick={() => {resetClientInfo(); handleClose();}}>
+            Fechar
+          </Buttons>
+        </ModalCar.Footer>
+      </ModalCar>
+
+      {/* Segundo Modal - ADD Informações do Cliente */}
+      <ModalCar show={showSecondModal} onHide={handleCloseSecondModal}>
+        <ModalCar.Header>
+          <ModalTitle>Confirmar Pedido</ModalTitle>
+        </ModalCar.Header>
+        <ModalCar.Body>
+          <InfoPedido>
+            Por Favor, Preencha Suas Informações Para Finalizar o Pedido!
+          </InfoPedido>
+          <ContentInfo>
+            <PedidoInput
+              type="text"
+              name="name"
+              value={clientInfo.name}
+              onChange={handleInputChange}
+              placeholder="Nome"
+            />
+            <PedidoInput
+              type="tel"
+              name="phone"
+              value={clientInfo.phone}
+              onChange={handleInputChange}
+              placeholder="Telefone"
+            />
+          </ContentInfo>
+          <h3>Total: R$ {totalPrice},00</h3>
+        </ModalCar.Body>
+        <ModalCar.Footer>
+          <Buttons variant="primary" onClick={handleSubmit}>
+            Confirmar Pedido
+          </Buttons>
+        </ModalCar.Footer>
+      </ModalCar>
+
+      {/* Terceiro Modal - Confirmar Pedido */}
+      <ModalCar show={showConfirmationModal} onHide={handleCloseConfirmationModal}>
+          <ModalCar.Header>
+            <ModalTitle>Resumo Do Pedido</ModalTitle>
+          </ModalCar.Header>
+          <ModalCar.Body>
+            <TitleProdutos>Detalhes Do Pedido</TitleProdutos>
+            <InfoPedido>Nome: {clientInfo.name}</InfoPedido>
+            <InfoPedido>Telefone: {clientInfo.phone}</InfoPedido>
+            <InfoPedido>Método de Entrega: {deliveryMethod}</InfoPedido>
+            {deliveryMethod === 'Delivery' && (
+              <>
+                <TitleProdutos>Endereço</TitleProdutos>
+                <InfoPedido>{clientInfo.street}, {clientInfo.number}</InfoPedido>
+                <InfoPedido>{clientInfo.neighborhood} - {clientInfo.complement}</InfoPedido>
+              </>
+            )}
+            <TitleProdutos>Produtos</TitleProdutos>
+            {cart.map((product, index) => (
+              <div key={index}>
+                  <InfoPedido>{product.nameProduct} - R$ {product.price},00</InfoPedido>
+              </div>
+            ))}
+            <h3>Total: R$ {totalPrice},00</h3>
+            {deliveryMethod === 'Delivery' && (
+              <MessageTaxa>*Consultar Taxa de Entrega Após Envio do Pedido*</MessageTaxa>
+            )}
+          </ModalCar.Body>
+          <ModalCar.Footer>
+            <Buttons variant="primary" onClick={handleCloseConfirmationModal}>
+              Finalizar Pedido
+            </Buttons>
+          </ModalCar.Footer>
+      </ModalCar>
+    </>
   );
 };
 
--- capixaba/src/Components/Modal/style.js
+++ capixaba/src/Components/Modal/style.js
@@ -1,86 +1,91 @@
 import styled from "styled-components";
-import { Button, Modal } from "react-bootstrap";
+import { Button, InputGroup, Modal } from "react-bootstrap";
 
 export const ModalCar = styled(Modal)`
-
-  .modal-dialog{
-    max-width: 50vw;
+  .modal-dialog {
+    max-width: 50vw; /* Estilização do Modal Completo/Container */
     max-height: 50vh;
     margin: auto;
 
-    @media (max-width: 760px) {  
+    @media (max-width: 760px) {
       max-width: 80vw;
-      max-height: 70vh; 
+      max-height: 70vh;
     }
-
   }
 
   .modal-content {
-    border-radius: 10px; /* Borda arredondada */
+    /* Estilização do conteudo do modal */
+    border-radius: 10px;
     width: 100%;
     height: 100%;
     margin: 0;
     display: flex;
-    
   }
 
-  .modal-title{
+  .modal-title {
+    /* Estilização do titulo do modal */
     font-size: 35px;
     font-family: "Lucida Console", "Courier New", monospace;
     font-weight: bold;
     text-align: center;
     margin: 0 auto;
     width: 100%;
-    
-    @media (max-width: 475px) {  
+
+    @media (max-width: 475px) {
       font-size: 25px;
     }
   }
 
-  .modal-body{
+  .modal-body {
+    /* Estilização do corpo do modal */
     font-size: 20px;
-    @media(max-width: 875px ){
-    font-size: 17px;
-  }
+    @media (max-width: 875px) {
+      font-size: 17px;
+    }
   }
   .modal-header {
+    /* Estilização do cabeçalho do modal */
     border-bottom: none; /* Remove a borda inferior */
     justify-content: center;
     display: flex;
     align-items: center;
-    width: 100% ;
+    width: 100%;
     text-align: center;
     position: relative;
   }
 
   .modal-footer {
+    /* Estilização do rodapé do modal */
     border-top: none; /* Remove a borda superior */
   }
 `;
 
 export const ContentCar = styled.div`
+  /* Estilização dos conteudos do carrinho/content */
   width: 100%;
   border-top: 1px solid black;
   padding-top: 8px;
   display: flex;
   align-items: center;
 
-  .titleProduct{
+  .titleProduct {
     font-size: 30px;
   }
-`
+`;
 export const ContainerInput = styled.div`
+  /* Estilização container do input de retirada e delivery */
   display: flex;
   flex-direction: row;
   width: 100%;
   margin: 20px 0;
   justify-content: center;
   text-align: center;
-  align-items:center;
-  gap: 20px; 
-`
+  align-items: center;
+  gap: 20px;
+`;
 
 export const RadioInput = styled.input`
+  /* Estilização do input de retirada e delivery */
   display: none;
 
   &:checked + label {
@@ -88,61 +93,110 @@ export const RadioInput = styled.input`
     color: white; /* Cor do texto quando selecionado */
     border-radius: 5px; /* Borda arredondada */
   }
-`
+`;
 
 export const RadioLabel = styled.label`
+  /* Estilização do label de retirada e delivery */
   padding: 10px 15px;
   width: 25%;
   margin-top: 5px;
-  border: 1px solid #007bff; /* Borda do botão */
-  border-radius: 5px; /* Borda arredondada */
-  cursor: pointer; /* Cursor de pointer ao passar o mouse */
+  border: 1px solid #007bff;
+  border-radius: 5px;
+  cursor: pointer;
   transition: background-color 0.3s;
   font-size: 15px;
 
   &:hover {
-    background-color: #0056b3; /* Cor de fundo ao passar o mouse */
-    color: white; /* Cor do texto ao passar o mouse */
+    background-color: #0056b3;
+    color: white;
   }
 
-  @media(max-width: 455px ){
+  @media (max-width: 455px) {
     font-size: 11px;
-    width:25%;
+    width: 25%;
   }
 `;
 
 export const Buttons = styled(Button)`
+  /* Estilização do botao de fechar e avancar */
   width: 25%;
 
-  @media(max-width: 510px ){
-    width: 30%
+  @media (max-width: 510px) {
+    width: 30%;
   }
-  
-  @media(max-width: 390px ){
+
+  @media (max-width: 390px) {
     font-size: 9px;
   }
-`
+`;
 export const ButtonRemove = styled.p`
+  /* Estilização do botao de remover o item do carrinho */
   display: block;
   width: 100%;
   margin: auto;
   cursor: pointer;
   font-size: 25px;
 
-  &:hover{
+  &:hover {
     color: red;
   }
-`
+`;
 
 export const ContainerButton = styled.div`
+  /* Estilização do container do  botao de remover o item do carrinho */
   display: flex;
   width: 10%;
   align-items: right;
 
-  justify-content: center; 
+  justify-content: center;
   margin: auto;
-`
+`;
 export const SubContainer = styled.div`
+  /* Estilização do subcontainer do  botao de remover o item do carrinho */
   display: flex;
   width: 80%;
+`;
+
+export const ContainerEndereco = styled(InputGroup)`
+  align-items: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`
+
+export const PedidoInput = styled.input`
+ /* Estilização do input de endereco do carrinho */
+  padding: 10px;
+  width: 40%;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 15px;
+  margin-top: 5px;
+  cursor: pointer;
+
+  @media (max-width: 510px) {
+    font-size: 10px;
+  }
+`;
+
+export const MessageTaxa = styled.p`
+  color: red;
+`
+
+export const InfoPedido = styled.p`
+  font-weight: bold;
+  font-family: "Lucida Console", "Courier New", monospace;
+`
+
+export const ContentInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 10px;
+`
+export const TitleProdutos = styled.h3`
+  color: red;
+  font-weight: bold;
+  font-family: "Lucida Console", "Courier New", monospace;
 `
